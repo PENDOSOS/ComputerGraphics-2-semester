87088cdb2394e138d2dffb6493da3a859c915b3a
PENDOSOS <ivanpuchkin@gmail.com>
2025-02-06 21:13:36 +0300

    render.h

---
+++ Lab1/Render.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <dxgi.h>
+#include <d3d11.h>
+
+class Render
+{
+public:
+    Render()
+        : m_pDevice(nullptr)
+        , m_pDeviceContext(nullptr)
+        , m_pSwapChain(nullptr)
+        , m_pBackBufferRTV(nullptr)
+        , m_width(16)
+        , m_height(16)
+    {}
+
+    bool init(HWND hWnd);
+    void terminate();
+
+    bool render();
+    bool resize(UINT width, UINT height);
+
+private:
+    HRESULT setupBackBuffer();
+
+private:
+    ID3D11Device* m_pDevice;
+    ID3D11DeviceContext* m_pDeviceContext;
+
+    IDXGISwapChain* m_pSwapChain;
+    ID3D11RenderTargetView* m_pBackBufferRTV;
+
+    UINT m_width;
+    UINT m_height;
+};
+
